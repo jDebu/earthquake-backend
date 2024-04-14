0af4810e3e0fb366455e87adabd9dc5346924555
manuel delgado <mdelgado.pcm@gmail.com>
2024-04-14 12:56:47 -0500

    chore: comment programming test

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,6 @@ every 1.day, at: '12:00 am' do
   rake "earthquake_data:fetch"
 end
 # test
-every 2.minutes do
-  rake "earthquake_data:fetch"
-end
+# every 2.minutes do
+#   rake "earthquake_data:fetch"
+# end
